9ec2046e80f85333fbf81c074b1769dc76011bb9
Ronen <RonenNess@gmail.com>
2017-07-01 01:10:02 +0200

    added cpu-animated entity to skinned mesh example

--- SkinnedMesh/Game1.cs
+++ SkinnedMesh/Game1.cs
@@ -15,6 +15,9 @@ namespace EmptyGeonBitProject
     /// </summary>
     internal class Game1 : GeonBitGame
     {
+        // paragraph to show diagnostic data
+        GeonBit.UI.Entities.Paragraph DiagnosticData;
+
         /// <summary>
         /// Initialize your GeonBitGame properties here.
         /// </summary>
@@ -49,24 +52,65 @@ namespace EmptyGeonBitProject
             // create the scene
             GameScene scene = new GameScene();
 
-            // create skinned mesh object
-            GameObject modelObject = new GameObject("soldier");
-            SkinnedModelRenderer model = new SkinnedModelRenderer("game/mech/Mech");
-
-            // set model materials color and disable lighting
-            foreach (var material in model.GetMaterials())
+            // Added GPU-animated model
             {
-                material.LightingEnabled = false;
-                material.DiffuseColor = Color.Gray;
-                material.SpecularColor = Color.White;
-                material.SpecularPower = 50f;
+
+                // create skinned mesh object
+                GameObject modelObject = new GameObject("mech");
+                SkinnedModelRenderer model = new SkinnedModelRenderer("game/mech/Mech");
+
+                // set model materials color and disable lighting
+                foreach (var material in model.GetMaterials())
+                {
+                    material.LightingEnabled = true;
+                    material.DiffuseColor = Color.Gray;
+                    material.SpecularColor = Color.White;
+                    material.SpecularPower = 50f;
+                    material.AmbientLight = new Color(150, 180, 225);
+                }
+
+                // add to gameobject and set its position and rotation
+                modelObject.AddComponent(model, "model");
+                modelObject.SceneNode.Scale = Vector3.One * 0.1f;
+                modelObject.SceneNode.PositionX = -45;
+                modelObject.SceneNode.RotationY = (float)System.Math.PI;
+                modelObject.Parent = scene.Root;
+
             }
 
-            // add to gameobject and set its position and rotation
-            modelObject.AddComponent(model, "model");
-            modelObject.SceneNode.Scale = Vector3.One * 0.1f;
-            modelObject.SceneNode.RotationY = (float)System.Math.PI;
-            modelObject.Parent = scene.Root;
+            // Added CPU-animated model
+            {
+
+                // create skinned mesh object
+                GameObject modelObject = new GameObject("wolf");
+                SkinnedModelRenderer model = new SkinnedModelRenderer("game/wolf/Wolf");
+                model.RenderingQueue = GeonBit.Core.Graphics.RenderingQueue.Mixed;
+                model.SetClip("Wolf_Skeleton|Wolf_Walk_cycle_", transitionTime: 0f);
+
+                // set model materials color and disable lighting
+                foreach (var material in model.GetMaterials())
+                {
+                    material.DiffuseColor = Color.White;
+                    material.AmbientLight = Color.Gray;
+                    material.Alpha = 1f;
+                }
+
+                // fix something in wolf's model (problem with one of the mesh effects)
+                var furMaterial = model.GetMaterial("Wolf_obj_fur", 0);
+                furMaterial.Alpha = 1f;
+                furMaterial.DiffuseColor = Color.White;
+                furMaterial.SpecularPower = 10f;
+                furMaterial.SpecularColor = Color.Black;
+                furMaterial.Texture = Resources.GetTexture("game/wolf/textures/Wolf_Fur_Alpha");
+
+                // add to gameobject and set its position and rotation
+                modelObject.AddComponent(model, "model");
+                modelObject.SceneNode.Scale = Vector3.One * 1.5f;
+                modelObject.SceneNode.PositionX = 45;
+                modelObject.SceneNode.RotationY = (float)System.Math.PI;
+                modelObject.Parent = scene.Root;
+
+            }
 
             // create skybox
             Managers.GraphicsManager.CreateSkybox(null, scene.Root);
@@ -80,8 +124,12 @@ namespace EmptyGeonBitProject
             camera.SceneNode.RotationX = -2.5f;
             camera.Parent = scene.Root;
 
+            // add diagnostic data paragraph to scene
+            DiagnosticData = new GeonBit.UI.Entities.Paragraph("", GeonBit.UI.Entities.Anchor.BottomLeft, scale:0.75f, offset: new Vector2(20, 20));
+            scene.UserInterface.AddEntity(DiagnosticData);
+
             // set scene
-            GeonBitMain.Instance.Application.LoadScene(scene);
+            scene.Load();
         }
 
         /// <summary>
@@ -90,8 +138,7 @@ namespace EmptyGeonBitProject
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         override public void Draw(GameTime gameTime)
         {
-            /// TBD add any custom drawing functionality here.
-            /// Note: since GeonBit handle its own drawing internally, usually you don't need to do anything here.
+            DiagnosticData.Text = Managers.Diagnostic.GetReportString();
         }
     }
 }
